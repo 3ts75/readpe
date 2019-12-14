082e392deeb7b48e685e75ce20817266fe8146e3
3ts75 <bimy1280163@gn.iwasaki.ac.jp>
2019-12-14 17:17:51 +0900

    Section Header

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,79 @@
 extern crate clap;
 use clap::{App, Arg, SubCommand};
+use std::env;
 mod pe;
 
-fn main() -> Result<(), Box<std::error::Error>> {
-    let app = App::new("clapex")
-        .arg(Arg::with_name("COFF-File-Header")
-        .help("Basic information about the file and pointers to other structures")
-        .short("h")
-        .long("coff-file-header")
-        .takes_value(true)
-        )
-        .arg(Arg::with_name("Optional-Header")
-        .help("Additional information about file execution")
-        .short("o")
-        .long("optional-header")
-        .takes_value(true)
-    );
+fn red_color(buf: Vec<u8>, i: usize) {
+    println!("\x1b[38;5;00196mOffset 0x{:X} must be 0x01 or 0x02\nUnknown magic 0x{:02x}.\x1b[m", i, buf[i]);
+}
 
-    let buf: Vec<u8> = read_file("hello_PE_x64.exe".to_string())?;
-    let mut i = 0;
-    while buf[i] != 0x50 || buf[i+1] != 0x45 || buf[i+2] != 0x00 || buf[i+3] != 0x00 {
-        i += 1;
+fn main() -> Result<(), Box<std::error::Error>> {    
+    let args: Vec<String> = env::args().collect();
+
+    let option = args[1].as_str();
+    let file_name: &str = &args[2];
+
+    if args.len() == 3 {
+        let buf: Vec<u8> = read_file(file_name.to_string())?;
+        let mut i = 0;
+        while buf[i] != 0x50 || buf[i+1] != 0x45 || buf[i+2] != 0x00 || buf[i+3] != 0x00 {
+            i += 1;
+        }
+        i += 4;
+
+        match option {
+            "-h" => pe::both_header::coff_file_header_detail(buf, i),
+            "-o" => match buf[i+0x15] {
+                0x01 => pe::pe86_header::optional_header_pe32_detail(buf, i),
+                0x02 => pe::pe64_header::optional_header_pe32_plus_detail(buf, i),
+                _ => red_color(buf, i+0x15),
+            },
+            "-S" => match buf[i+0x15] {
+                0x01 => pe::section_header::section_header_x86(buf, i),
+                0x02 => pe::section_header::section_header_x64(buf, i),
+                _ => red_color(buf, i+0x15),
+            }
+            _ => println!("wtf"),
+        }
     }
-    i += 4;
 
-    let matches = app.get_matches();
+    // let buf: Vec<u8> = read_file("hello_PE_x64.exe".to_string())?;
+    // let mut i = 0;
+    // while buf[i] != 0x50 || buf[i+1] != 0x45 || buf[i+2] != 0x00 || buf[i+3] != 0x00 {
+    //     i += 1;
+    // }
+    // i += 4;
 
-    if let Some(o) = matches.value_of("COFF-File-Header") {
-        pe::both::coff_file_header_detail(buf.clone(), i);
-    }
+    // let matches = app.get_matches();
 
-    if let Some(o) = matches.value_of("Optional-Header") {
-        i += 0x14;
-        match buf[i+1] {
-            0x01 => pe::pe86::optional_header_pe32_detail(buf.clone(), i),
-            0x02 => pe::pe64::optional_header_pe32_plus_detail(buf.clone(), i),
-            _ => println!("hoge"),
-        };
-    }
+    // if let Some(o) = matches.value_of("COFF-File-Header") {
+    //     pe::both_header::coff_file_header_detail(buf.clone(), i);
+    // }
+
+    // if let Some(o) = matches.value_of("Optional-Header") {
+    //     i += 0x14;
+    //     match buf[i+1] {
+    //         0x01 => pe::pe86_header::optional_header_pe32_detail(buf.clone(), i),
+    //         0x02 => pe::pe64_header::optional_header_pe32_plus_detail(buf.clone(), i),
+    //         _ => println!("hoge"),
+    //     };
+    // }
+
+    // if let Some(o) = matches.value_of("Section-Header") {
+    //     match buf[i+0x15] {
+    //         0x01 => pe::section_header::section_header_x86(buf.clone(), i),
+    //         0x02 => pe::section_header::section_header_x64(buf.clone(), i),
+    //         _ => println!("hoge"),
+    //     }
+    // }
+
+    // if let Some(o) = matches.value_of("Section-Header-Characteristics") {
+    //     match buf[i+0x15] {
+    //         0x01 => pe::sh_characteristics::section_header_x86(buf.clone(), i),
+    //         0x02 => pe::sh_characteristics::section_header_x64(buf.clone(), i),
+    //         _ => println!("hoge"),
+    //     }
+    // }
     Ok(())
 }
 
--- src/pe/details/dll_characteristics.rs
+++ src/pe/details/dll_characteristics.rs
@@ -1,106 +1,45 @@
 type Word = u16;  // 2bytes
+use std::collections::HashMap;
 
-const IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA:  Word = 0x0020;  // Image can handle a high enropy 64-bit virtual address space.
-const IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE:  Word = 0x0040;  // DLL can be relocated at load time.
-const IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY:  Word = 0x0080;  // Code Integrity checks are enforced.
-const IMAGE_DLLCHARACTERISTICS_NX_COMPAT:  Word = 0x0100;  // Image is NX compatible.
-const IMAGE_DLLCHARACTERISTICS_NO_ISOLATION:  Word = 0x0200;  // Isolation aware, but do not isolate the image.
-const IMAGE_DLLCHARACTERISTICS_NO_SEH:  Word = 0x0400;  // Does not use structured exception (SE) handling. No SE handler may be called in this image.
-const IMAGE_DLLCHARACTERISTICS_NO_BIND:  Word = 0x0800;  // Do not bind the image.
-const IMAGE_DLLCHARACTERISTICS_APPCONTAINER:  Word = 0x1000;  // Image must execute in an AppContainer.
-const IMAGE_DLLCHARACTERISTICS_WDM_DRIVER:  Word = 0x2000;  // A WDM driver.
-const IMAGE_DLLCHARACTERISTICS_GUARD_CF:  Word = 0x4000;  // Image supports Control Flow Guard.
-const IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE:  Word = 0x8000;  // Terminal Server aware.
+// const IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA:  Word = 0x0020;  // Image can handle a high enropy 64-bit virtual address space.
+// const IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE:  Word = 0x0040;  // DLL can be relocated at load time.
+// const IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY:  Word = 0x0080;  // Code Integrity checks are enforced.
+// const IMAGE_DLLCHARACTERISTICS_NX_COMPAT:  Word = 0x0100;  // Image is NX compatible.
+// const IMAGE_DLLCHARACTERISTICS_NO_ISOLATION:  Word = 0x0200;  // Isolation aware, but do not isolate the image.
+// const IMAGE_DLLCHARACTERISTICS_NO_SEH:  Word = 0x0400;  // Does not use structured exception (SE) handling. No SE handler may be called in this image.
+// const IMAGE_DLLCHARACTERISTICS_NO_BIND:  Word = 0x0800;  // Do not bind the image.
+// const IMAGE_DLLCHARACTERISTICS_APPCONTAINER:  Word = 0x1000;  // Image must execute in an AppContainer.
+// const IMAGE_DLLCHARACTERISTICS_WDM_DRIVER:  Word = 0x2000;  // A WDM driver.
+// const IMAGE_DLLCHARACTERISTICS_GUARD_CF:  Word = 0x4000;  // Image supports Control Flow Guard.
+// const IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE:  Word = 0x8000;  // Terminal Server aware.
 
-fn red_color(str: String) {
-    println!("      ├─\x1b[38;5;00196m{}\x1b[m", str);
+fn red_color(s: &str) {
+    println!("      ├─\x1b[38;5;00196m{}\x1b[m", s);
 }
 
-fn green_color(str: String) {
-    println!("      ├─\x1b[38;5;0046m{}\x1b[m", str);
-}
-
-fn yellow_color(str: String) {
-    println!("      ├─\x1b[38;5;0011m{}\x1b[m", str);
+fn green_color(s: &str) {
+    println!("      ├─\x1b[38;5;0046m{}\x1b[m", s);
 }
 
 pub fn dll_characteristics(flag: Word) {
-    let check = |x| {flag & x == x};
+    let mut IMAGE_DLLCHARACTERISTICS = HashMap::new();
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA"), 0x0020);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE"), 0x0040);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY"), 0x0080);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_NX_COMPAT"), 0x0100);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_NO_ISOLATION"), 0x0200);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_NO_SEH"), 0x0400);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_NO_BIND"), 0x0800);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_APPCONTAINER"), 0x1000);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_WDM_DRIVER"), 0x2000);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_GUARD_CF"), 0x4000);
+    IMAGE_DLLCHARACTERISTICS.insert(String::from("IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE"), 0x8000);
 
-    // 0x0020
-    if check(IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA) {
-        red_color("IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA".to_string());
-    }
-
-    // 0x0040
-    if check(IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE) {
-        red_color("IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE".to_string());
-    }
-
-    // 0x0080
-    if check(IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY) {
-        red_color("IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY".to_string());
-    }
-
-    // 0x0100
-    if check(IMAGE_DLLCHARACTERISTICS_NX_COMPAT) {
-        red_color("IMAGE_DLLCHARACTERISTICS_NX_COMPAT".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_NX_COMPAT".to_string());
-    }
-
-    // 0x0200
-    if check(IMAGE_DLLCHARACTERISTICS_NO_ISOLATION) {
-        red_color("IMAGE_DLLCHARACTERISTICS_NO_ISOLATION".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_NO_ISOLATION".to_string());
-    }
-
-    // 0x0400
-    if check(IMAGE_DLLCHARACTERISTICS_NO_SEH) {
-        red_color("IMAGE_DLLCHARACTERISTICS_NO_SEH".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_NO_SEH".to_string());
-    }
-
-    // 0x0800
-    if check(IMAGE_DLLCHARACTERISTICS_NO_BIND) {
-        red_color("IMAGE_DLLCHARACTERISTICS_NO_BIND".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_NO_BIND".to_string());
-    }
-
-    // 0x1000
-    if check(IMAGE_DLLCHARACTERISTICS_APPCONTAINER) {
-        red_color("IMAGE_DLLCHARACTERISTICS_APPCONTAINER".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_APPCONTAINER".to_string());
-    }
-
-    // 0x2000
-    if check(IMAGE_DLLCHARACTERISTICS_WDM_DRIVER) {
-        red_color("IMAGE_DLLCHARACTERISTICS_WDM_DRIVER".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_WDM_DRIVER".to_string());
-    }
-
-    // 0x4000
-    if check(IMAGE_DLLCHARACTERISTICS_GUARD_CF) {
-        red_color("IMAGE_DLLCHARACTERISTICS_GUARD_CF".to_string());
-    } else {
-        green_color("IMAGE_DLLCHARACTERISTICS_GUARD_CF".to_string());
-    }
-
-    // 0x8000
-    if check(IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE) {
-        println!("      └─\x1b[38;5;0011mThis flag is must be zeroIMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE\x1b[m");
-    } else {
-        println!("      └─\x1b[38;5;0046mIMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE\x1b[m");
+    for (flag_name, flag_bit) in &IMAGE_DLLCHARACTERISTICS {
+        if flag & *flag_bit == *flag_bit {
+            red_color(flag_name);
+        } else {
+            green_color(flag_name);
+        }
     }
 }
